79b3eda06a7c4c685912e20fd2c3cec68d7d551d
mutur4 <8421gilbertrobert@gmail.com>
2021-02-19 07:19:16 -0500

    Update asd.py

--- CyberSpaceKenya/thematatu/asd.py
+++ CyberSpaceKenya/thematatu/asd.py
@@ -11,14 +11,16 @@ def main():
         
         flag_ = [claripy.BVS(f"flag_{i}", 8) for i in range(23)] #create a flag bitvector symbol
         flag = claripy.Concat( *flag_) #unpack the flag list
-
-        state = p.factory.blank_state(addr=start_address) #create state
+        
+        state = p.factory.blank_state(addr=start_address) 
         state.regs.rdx = 0x17
         state.regs.rcx = 0xc4200163a0 # remember out input was stored in rcx 
         state.memory.store(0xc4200163a0, flag)
-
+        
+        # here we create a simulation manager
+        # we will use this sim manager to find the address of main.win() and avoid some adddresses including main.fail()
         sim = p.factory.simgr(state)
-        sim.explore(find=0x48e6a0, avoid=[0x48e88d, 0x48ea6d]) # give the address to avoid and address to find. We will find the address of main.win()
+        sim.explore(find=0x48e6a0, avoid=[0x48e88d, 0x48ea6d])
 
         if sim.found:
                 print(sim.found[0].solver.eval(flag, cast_to=bytes)) #get the flag \0/
